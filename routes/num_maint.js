fae14a6353922df9542c51358102013bab3b525e
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-12 12:30:41 -0400

    Update num-maint properly, couchdb PUT needs _rev

--- routes/num_maint.js
+++ routes/num_maint.js
@@ -16,31 +16,50 @@ router.get('/', function(req, res) {
 	    .map(function(x) { return x[0]; });
 	var num = Object.keys(array_uniq(maints)).length;
 
-	var url = dburl
+	var url2 = dburl
 	    .replace(':user:', dbuser)
 	    .replace(':pass:', dbpass) +
 	    '/num-maint';
 
-	var data = { '_id' : 'num_maint',
-		     'type': 'data',
-		     'count': num };
-	
-	request(
-	    { url: url,
-	      method: 'PUT',
-	      json: data },
-	    function(error, response, body) {
-		if (error || (response.statusCode != 200 &&
-			      response.statusCode != 201)) {
-		    return res.render('error');
-		}
-		
-		res.set('Content-Type', 'application/json');
-		res.send({ 'result': 'ok' });
-		res.end();
+	request(url2, function(error, response, body) {
+
+	    if (error || response.statusCode != 200) {
+		return res.render('error');
 	    }
-	)
+
+	    var doc = JSON.parse(body);
+	    var oldnum = doc["count"];
+
+	    if (oldnum == num) { return result_ok(res); }
+
+	    var rev = doc["_rev"];
+	    var data = { '_id' : 'num_maint',
+			 '_rev': rev,
+			 'type': 'data',
+			 'count': num };
+
+	    request(
+		{ url: url2,
+		  method: 'PUT',
+		  json: data },
+
+		function(error, response, body) {
+		    if (error || (response.statusCode != 200 &&
+				  response.statusCode != 201)) {
+			return res.render('error');
+		    }
+
+		    return result_ok(res);
+		}
+	    )
+	})
     })
 })
+
+function result_ok(res) {
+    res.set('Content-Type', 'application/json');
+    res.send({ 'result': 'ok' });
+    res.end();
+}
 
 module.exports = router;
